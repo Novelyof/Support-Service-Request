5c88519d359e981dedfbbeaf48cf7f76bbd8796f
Noveliyo Frendika <78901124+Novelyof@users.noreply.github.com>
2023-11-28 02:20:17 +0000

    update

--- app/submit/page.js
+++ app/submit/page.js
@@ -1,50 +1,53 @@
 import styles from '../styles/Submit.module.css';
+import Image from 'next/image';
 
 const Submit = () => {
     return (
         <div className={styles.backgroundImage}>
             <div className={styles.headerBox}>
                 <div className={styles.backButton}>
-                    <a href="/home">&#8592; Back to Home</a>
+                    <a href="/home">Back</a>
                 </div>
                 <h1 className={styles.headerText}>Hello Yulio, What’s Your Problem? You Can Fill This Form</h1>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            src="/GambarSubmit.png" // Ganti dengan path gambar Anda
+                            alt="Description of the image"
+                            width={250}
+                            height={250}
+                        />
+                    </div>
             </div>
-            <div className={styles.inputSection}>
-                <div className={styles.basicAndStatus}>
-                    <div className={styles.basicInformation}>
-                        <h2>Basic Information</h2>
-                        <input type="text" placeholder="Name" />
-                        <input type="text" placeholder="Email" />
-                        <input type="text" placeholder="Phone Number" />
-                        <input type="text" placeholder="Phone Number" />
+
+            <div className={styles.content}>
+                <div className={styles.container}>
+                    <h2 className={styles.title}>Basic Information</h2>
+                    <div className={styles.columnsContainer}>
+                        <div className={styles.column}>
+                            <label>Label 1:</label>
+                            <input type="text" placeholder="First Name" className={styles.inputField} />
+                            <label>Label 3:</label>
+                            <input type="text" placeholder="Last Name" className={styles.inputField} />
+                        </div>
+                        <div className={styles.column}>
+                            <label>Label 2:</label>
+                            <input type="text" placeholder="Email" className={styles.inputField} />
+                            <label>Label 4:</label>
+                            <input type="text" placeholder="Phone Number" className={styles.inputField} />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.statusContainer}>
+                    <div className={styles.draftField}>
+                        Status Request 
                     </div>
                     <div className={styles.statusRequest}>
-                        <h2>Status Request</h2>
-                        <input type="text" value="Draft" disabled />
-                    </div>
-                </div>
-                <div className={styles.detailInformation}>
-                    <h2>Detail Information Request</h2>
-                    <input type="text" placeholder="Field Atas Tengah" />
-                    <div className={styles.columnsWrapper}>
-                        <div className={styles.column}>
-                            <input type="text" placeholder="Field 1" />
-                            <input type="text" placeholder="Field 4" />
-                            <input type="text" placeholder="Field 7" />
-                        </div>
-                        <div className={styles.column}>
-                            <input type="text" placeholder="Field 2" />
-                            <input type="text" placeholder="Field 5" />
-                            <input type="text" placeholder="Field 8" />
-                        </div>
-                        <div className={styles.column}>
-                            <input type="text" placeholder="Field 3" />
-                            <input type="text" placeholder="Field 6" />
-                            <input type="text" placeholder="Field 9" />
-                        </div>
+                        Draft
                     </div>
                 </div>
             </div>
+            
+            
         </div>
     );
 };
